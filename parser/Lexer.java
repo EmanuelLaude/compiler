76bcd49fd8c536cbef62116a4e16fc96c19f1c20
Emanuel Laude <emanuellaude@Emanuels-MacBook-Pro.local>
2015-12-13 12:39:03 +0100

    removed files

--- parser/Lexer.java
+++ parser/Lexer.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package compiler;
+package parser;
 
 import java.text.ParseException;
 import java.util.Collections;
--- parser/Tree.java
+++ parser/Tree.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package compiler;
+package parser;
 
 import java.io.IOException;
 import java.io.PrintStream;
